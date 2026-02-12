95f6789b17b376d2d66a4f5410d6e983dc8df68d
James Graham <james@hoppipolla.co.uk>
2026-02-12 09:16:14 +0000

    Update templates for Interop 2026

--- gh_comment/responses.py
+++ gh_comment/responses.py
@@ -1,19 +1,16 @@
-REJECT_TEMPLATE = """Thank you for proposing {title} for inclusion in Interop 2025.
+REJECT_TEMPLATE = """"Thank you for proposing {title} for Interop 2026.
 
-We wanted to let you know that this proposal was not selected to be part of Interop this year.
+We want to let you know it was not selected to be part of Interop this year.
 
-On behalf of the entire Interop team, thank you for submitting this proposal for consideration. We got many more proposals than we could include in this year's project, necessitating some difficult choices. Please note this should not be taken as a comment on the technology as a whole, or our willingness to consider it for Interop in the future. We appreciate the work you put into your proposal, and would welcome your participation in future rounds of Interop.
+On behalf of the entire Interop team, thank you for submitting it for consideration. We got many more proposals than we could include in this year's project, necessitating some difficult choices.  Please note this should not be taken as a comment on the technology as a whole, or our willingness to consider it for Interop in the future.  We appreciate the work you put into your proposal, and would welcome your participation in future rounds of Interop.
 
-For an overview of our process, see [proposal selection](https://github.com/web-platform-tests/interop/blob/main/2025/selection-process.md). Thank you again for contributing to Interop 2025.
-
-_Posted on behalf of the Interop team._
+_Posted on behalf of the Interop team._"
 """
 
-ACCEPT_TEMPLATE = """Thank you for proposing {title} for inclusion in Interop 2025.
+ACCEPT_TEMPLATE = """Thank you for proposing {title} for Interop 2026.
 
-We are pleased to let you know that this proposal was accepted as part of the {focus_area} {type}. You can follow the progress of this {type} on the [Interop 2025 dashboard](https://wpt.fyi/interop-2025).
+We are pleased to let you know that it has been accepted as part of the {focus_area} {type}.
 
-For an overview of our process, see [proposal selection](https://github.com/web-platform-tests/interop/blob/main/2025/selection-process.md). Thank you for contributing to Interop 2025!
+You’ll be able to track progress on this topic throughout the year via the [Interop 2026 dashboard](https://wpt.fyi/interop-2026).
 
 _Posted on behalf of the Interop team._"""
- 
