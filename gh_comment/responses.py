8c833ca37351241811720c9fa8d0bcb3bee00bbe
James Graham <james@hoppipolla.co.uk>
2026-02-12 16:08:52 +0000

    Remove extra quotes

--- gh_comment/responses.py
+++ gh_comment/responses.py
@@ -1,11 +1,10 @@
-REJECT_TEMPLATE = """"Thank you for proposing {title} for Interop 2026.
+REJECT_TEMPLATE = """Thank you for proposing {title} for Interop 2026.
 
 We want to let you know it was not selected to be part of Interop this year.
 
 On behalf of the entire Interop team, thank you for submitting it for consideration. We got many more proposals than we could include in this year's project, necessitating some difficult choices.  Please note this should not be taken as a comment on the technology as a whole, or our willingness to consider it for Interop in the future.  We appreciate the work you put into your proposal, and would welcome your participation in future rounds of Interop.
 
-_Posted on behalf of the Interop team._"
-"""
+_Posted on behalf of the Interop team._"""
 
 ACCEPT_TEMPLATE = """Thank you for proposing {title} for Interop 2026.
 
